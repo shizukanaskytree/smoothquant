2002aa0af673eb2d3d1457d5efe129281735964c
shizukanaskytree <shizukanaskytree@gmail.com>
2023-09-12 06:30:38 +0000

    minor change

--- examples/setup_env.sh
+++ examples/setup_env.sh
@@ -3,8 +3,9 @@
 #-------------------------------------------------------------------------------
 
 ### docker pull
-
 docker pull nvcr.io/nvidia/pytorch:21.06-py3
+
+watch -n0.1 nvidia-smi
 
 #-------------------------------------------------------------------------------
 
@@ -27,17 +28,14 @@ cat /etc/os-release
 
 #-------------------------------------------------------------------------------
 
-### To clean all exited Docker containers, you can use a combination of Docker commands. Here's a step-by-step guide:
-docker rm $(docker ps -a -f status=exited -q)
-
-#-------------------------------------------------------------------------------
+### VSCode: Attach to a running container, Developing inside a Container
+### https://code.visualstudio.com/docs/devcontainers/attach-container
+### open path: /workspace/outside-docker/
 
 ### start the exited and attach to it
 CONTAINER_NAME=zip_model
 docker start $CONTAINER_NAME
 docker attach $CONTAINER_NAME
-
-#-------------------------------------------------------------------------------
 
 ### check all conda env in the docker
 conda info --env
@@ -45,15 +43,14 @@ conda activate smoothquant
 
 #-------------------------------------------------------------------------------
 
-### VSCode: Attach to a running container, Developing inside a Container
-### https://code.visualstudio.com/docs/devcontainers/attach-container
-### open path: /workspace/outside-docker/
-
-#-------------------------------------------------------------------------------
-
 ### stop a container
 CONTAINER_NAME=zip_model
 docker stop $CONTAINER_NAME
+
+#-------------------------------------------------------------------------------
+
+### To clean all exited Docker containers, you can use a combination of Docker commands. Here's a step-by-step guide:
+docker rm $(docker ps -a -f status=exited -q)
 
 #-------------------------------------------------------------------------------
 
@@ -77,19 +74,6 @@ conda activate smoothquant
 
 #-------------------------------------------------------------------------------
 
-cd $CONTAINER_DIR
-# git clone https://github.com/mit-han-lab/smoothquant ### 只在第一次执行
-
-cd smoothquant
-
-pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install transformers accelerate datasets zstandard
-
-# python setup.py install ### if I want to debug the src
-pip install -e .
-
-#-------------------------------------------------------------------------------
-
 ### We implement SmoothQuant INT8 inference for PyTorch with [CUTLASS](https://github.com/NVIDIA/cutlass)
 ### INT8 GEMM kernels, which are wrapped as PyTorch modules in [torch-int](https://github.com/Guangxuan-Xiao/torch-int).
 ### Please install [torch-int](https://github.com/Guangxuan-Xiao/torch-int) before running the SmoothQuant PyTorch INT8 inference.
@@ -99,14 +83,13 @@ cd ..
 # git clone --recurse-submodules https://github.com/Guangxuan-Xiao/torch-int.git ### 只在第一次执行
 
 cd torch-int
-conda install -c anaconda gxx_linux-64=9
+conda install -c anaconda gxx_linux-64=9 -y
 pip install -r requirements.txt
 source environment.sh
 
-nvidia-smi --query-gpu=compute_cap --format=csv
 ### change the compute capacity in build_cutlass.sh
 ### ONLY THIS LINE: cmake .. -DCUTLASS_NVCC_ARCHS='60;61;70;75;80' -DCUTLASS_ENABLE_TESTS=OFF -DCUTLASS_UNITY_BUILD_ENABLED=ON # nvcc warning : The 'compute_35', 'compute_37', 'compute_50', 'sm_35', 'sm_37' and 'sm_50' architectures are deprecated, and may be removed in a future release (Use -Wno-deprecated-gpu-targets to suppress warning).
-
+nvidia-smi --query-gpu=compute_cap --format=csv
 bash build_cutlass.sh
 
 ### 缺文件，解法：(1) copy-paste two missing files from your local specific folder OR (2) reinstall cuda driver on the docker
@@ -116,6 +99,19 @@ python setup.py install
 
 ### test torch-int.git
 python tests/test_linear_modules.py
+
+#-------------------------------------------------------------------------------
+
+cd $CONTAINER_DIR
+cd smoothquant-prj
+# git clone https://github.com/mit-han-lab/smoothquant ### 只在第一次执行
+cd smoothquant
+
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install transformers accelerate datasets zstandard
+
+# python setup.py install ### if I want to debug the src
+pip install -e .
 
 #-------------------------------------------------------------------------------
 
